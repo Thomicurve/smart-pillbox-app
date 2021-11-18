5947782ed76a6abf26b363afd8ac9243045fc62d
Alan Escarcha <alanescarcha11@gmail.com>
2021-11-18 01:19:32 -0300

    Ahora la aplicación busca reconectar con el bluetooth cuando se desconecta

--- hooks/useBT.js
+++ hooks/useBT.js
@@ -25,35 +25,56 @@ const useBT = () => {
         }
     }
 
+    async function researchPillbox() { //Funcion para intentar volver a conectarse al bt
+        let loopScan = setInterval(async () => {
+            if (!btConectado) { //si el bluetooth esta desconectado seguir escaneando cada 4seg
+                console.info("No se puedo conectar con el pastillero");
+                await checkBT_device();
+                await scanDevices();
+                await conectar();
+                await checkArduino_Connected();
+            } else { //cuando se conecte, eliminar el loopScan
+                clearInterval(loopScan);
+                return;
+            }
+        }, 4000);
+    }
+
     //1- Funcion Comprobar si el bluetooth del celular esta encendido y permitido
     async function checkBT_device() {
-        await BleManager.enableBluetooth()
-            .then(() => {
-                btCelular = true;
-                console.info("El bluetooth ya esta encendido");
-            })
-            .catch((error) => {
-                btCelular = false;
-                console.info("El usuario no permite activar el bluetooth");
-                console.error(error);
-            });
+        if (!btConectado) {
+            await BleManager.enableBluetooth()
+                .then(() => {
+                    btCelular = true;
+                    console.info("El bluetooth ya esta encendido");
+                })
+                .catch((error) => {
+                    btCelular = false;
+                    console.info("El usuario no permite activar el bluetooth");
+                    console.error(error);
+                });
+        }
     }
 
     //2- Funcion escanear dispositivos en 3seg
     async function scanDevices() {
-        await BleManager.scan([], 3, true).then(() => {
-            console.info("Empezando escaneo...");
-        }); //hacer que escanee hasta que encuentre el pastillero
-        await Delay(3000); //esperar 3 segundos
-        await BleManager.stopScan().then(() => { //Detener el escaneo
-            console.log("Scan stopped");
-        });
+        if (btCelular) {
+            await BleManager.scan([], 2, true).then(() => {
+                console.info("Empezando escaneo...");
+            }); //hacer que escanee hasta que encuentre el pastillero
+            await Delay(2000); //esperar 3 segundos
+            await BleManager.stopScan().then(() => { //Detener el escaneo
+                console.log("Scan stopped");
+            });
+        } else {
+            console.info("Antes de escanear se debe encender el bluetooth.");
+        }
     }
 
 
     //3- Funcion conectar al arduino
     async function conectar() {
-        await BleManager.connect(ID_HM10)
+        BleManager.connect(ID_HM10)
             .then(async () => {
                 // Success code
                 console.log("Connected");
@@ -72,7 +93,7 @@ const useBT = () => {
             });
     }
 
-    //4- Funcion revisar conexion con el celular y el arduino
+    //4- Funcion revisar conexion con el celular y el arduino y enviar dato apagar cada 300ms
     async function checkArduino_Connected() {
         await BleManager.isPeripheralConnected(
             ID_HM10,
@@ -81,9 +102,34 @@ const useBT = () => {
             btConectado = isConnected;
             if (isConnected) {
                 console.log("Peripheral is connected!");
-                setInterval(apagar, 300);
+                btConectado = true;
+                let loopApagar = setInterval(async () => { //Blucle apagar cada 300ms
+                    if (!btConectado) { //no hacer el blucle si el bt no esta conectado
+                        clearInterval(loopApagar);
+                        researchPillbox(); //revisar conexion con el arduino
+                    } else if (!boolFindSmartPillbox && btConectado == true) { //Si no se busca el pastillero enviar 0 para que no suene el pastillero
+                        const data = stringToBytes('0');
+                        await BleManager.write(
+                            ID_HM10,
+                            "0000ffe0-0000-1000-8000-00805f9b34fb",
+                            "0000ffe1-0000-1000-8000-00805f9b34fb",
+                            data
+                        )
+                            // .then(() => {
+                            //     // Success code
+                            //     console.log("Write: " + data);
+                            // })
+                            .catch((error) => {
+                                console.error(error);
+                                btConectado = false;
+                                // NOTIFIACION CELULAR DE PASTILLERO DESCONECTADO AQUI
+                            });
+                    } else if (boolFindSmartPillbox && btConectado == true) { //si se trata del switch buscar pastillero y bt siguen conectados
+                        return; //No enviar nada para que el pastillero suene
+                    }
+                }, 300);
             } else {
-                console.log("Peripheral is NOT connected!");
+                console.error("Peripheral is NOT connected!");
                 btConectado = false;
             }
         });
@@ -91,26 +137,29 @@ const useBT = () => {
 
     // Funcion Apagar BUZZER
     async function apagar() {
-        if (!boolFindSmartPillbox) { //Si no se busca el pastillero enviar 0 para que no suene el pastillero
-            const data = stringToBytes('0');
-            await BleManager.write(
-                ID_HM10,
-                "0000ffe0-0000-1000-8000-00805f9b34fb",
-                "0000ffe1-0000-1000-8000-00805f9b34fb",
-                data
-            )
-                // .then(() => {
-                //     // Success code
-                //     console.log("Write: " + data);
-                // })
-                .catch((error) => {
-                    console.error(error);
-                    btConectado = false;
-                    // NOTIFIACION CELULAR DE PASTILLERO DESCONECTADO AQUI
-                });
-        } else {
-            return; //No enviar nada para que el pastillero suene
-        }
+        // if(!btConectado) {
+        //     clearInterval(loopApagar);
+        //     researchPillbox(); //revisar conexion con el arduino
+        // }else if (!boolFindSmartPillbox && btConectado == true) { //Si no se busca el pastillero enviar 0 para que no suene el pastillero
+        //     const data = stringToBytes('0');
+        //     await BleManager.write(
+        //         ID_HM10,
+        //         "0000ffe0-0000-1000-8000-00805f9b34fb",
+        //         "0000ffe1-0000-1000-8000-00805f9b34fb",
+        //         data
+        //     )
+        //         // .then(() => {
+        //         //     // Success code
+        //         //     console.log("Write: " + data);
+        //         // })
+        //         .catch((error) => {
+        //             console.error(error);
+        //             btConectado = false;
+        //             // NOTIFIACION CELULAR DE PASTILLERO DESCONECTADO AQUI
+        //         });
+        // } else if(boolFindSmartPillbox && btConectado == true){ //si se trata del switch buscar pastillero y bt siguen conectados
+        //     return; //No enviar nada para que el pastillero suene
+        // }
     }
 
     function findSmartPillbox(isEnabled) {
@@ -135,19 +184,16 @@ const useBT = () => {
             await checkBT_device();
             if (btCelular) { //si el bluetooth esta encendido y permitido
                 await scanDevices();
+                await Delay(1000);
                 await conectar();
                 await checkArduino_Connected();
-                if (!btConectado) {
-                    console.error("No se puedo conectar con el pastillero");
-                    btConectado = false;
-                }
+                researchPillbox();
             } else {
                 console.error("El bluetooth no esta encendido");
                 btConectado = false;
             }
         }
         bluetooth();
-        console.log('working')
     }, [])
 
     return { findSmartPillbox, isEnabled, toggleSwitch }
